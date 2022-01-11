6824651e508c546da28d98146fce49a69fb743e7
Sarim Aleem <sarimaleem99@gmail.com>
2022-01-11 13:50:46 -0600

    created api for posting person

--- app.js
+++ app.js
@@ -1,15 +1,36 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const path = require('path')
+const express = require("express");
+const app = express();
+const port = 3000;
+const path = require("path");
+const sqlite3 = require("sqlite3");
+const db = new sqlite3.Database("people.db");
 
 // TODO: figure out how this works?
 // app.use(express.static('frontend/'))
+app.use(express.json());
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend/home.html"));
+});
 
-app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, 'frontend/home.html'))
-    })
+app.post("/", (req, res) => {
+    const user = req.body;
+    if (user.first === undefined || user.last === undefined) {
+        res.status(400).json({ msg: "bad body" });
+    } else {
+        db.run(
+            `insert into people (id, first, last) values (?, ?, ?);`,
+            parseInt(Math.random() * 1000000),
+            user.first,
+            user.last
+        );
+        db.all("select * from people", (err, rows) => {
+            console.log(rows);
+        });
+        res.status(201).json({ msg: "object created" });
+    }
+});
 
-app.listen(port, () => {port
-        console.log(`listening at ${port}`)
-    })
+app.listen(port, () => {
+    port;
+    console.log(`listening at ${port}`);
+});
---
+++ search/search_api.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const router = express.Router()
+
+router.search('/:id', () => {
+    // gonna go for non fuzzy search for now
+})
